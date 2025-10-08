c3d133241886cfba23e514c1400039ab2f552ea9
iwalkintotheroom <jdjohnson434@gmail.com>
2025-10-08 05:48:14 +0000

    added requirements.txt

--- personDetector.py
+++ personDetector.py
@@ -2,3 +2,12 @@ import ultralytics
 import cv2
 import numpy as np
 from ultralytics import YOLO
+
+imx_model = YOLO("yolo11n_imx_model")
+
+results = imx_model("https://ultralytics.com/images/bus.jpg")
+
+cv2.imshow("img", results[0].plot())
+cv2.waitKey(q)
+cv2.destroyAllWindows()
+
